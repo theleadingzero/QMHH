911bd174d5476907dd5b5ac9d1dc513dc2828c9e
Vincent Akkermans <vincent.akkermans@gmail.com>
2012-03-01 15:34:21 +0000

    eclipse processing setup

---
+++ src/net/qmat/qmhh/ProcessingObject.java
@@ -0,0 +1,11 @@
+package net.qmat.qmhh;
+import processing.core.*;
+
+public class ProcessingObject {
+	protected PApplet p;
+	
+	public ProcessingObject(PApplet p)
+	{
+		this.p = p;
+	}
+}
---
+++ src/net/qmat/qmhh/Stripe.java
@@ -0,0 +1,33 @@
+package net.qmat.qmhh;
+
+import processing.core.PApplet;
+
+public class Stripe extends ProcessingObject {
+	
+	float x;       // horizontal location of stripe
+	float speed;   // speed of stripe
+	float w;       // width of stripe
+	boolean mouse; // state of stripe (mouse is over or not?)
+
+	Stripe(PApplet p) {
+		super(p);
+		x = 0;              // All stripes start at 0
+		speed = p.random(1);  // All stripes have a random positive speed
+		w = p.random(10,30);
+		mouse = false;
+	}
+
+	// Draw stripe
+	void display() {
+		p.fill(255,100);
+		p.noStroke();
+		p.rect(x,0,w,p.height);
+	}
+
+	// Move stripe
+	void move() {
+		x += speed;
+		if (x > p.width+20) x = -20;
+	}
+
+}
