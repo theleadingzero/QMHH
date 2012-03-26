c7534edd0e5f6bffddccae9707c048fc1ff86131
ireti olowe <thinctank@yahoo.com>
2012-03-26 20:11:47 +0100

    added robot

--- src/net/qmat/qmhh/models/creatures/Robot.java
+++ src/net/qmat/qmhh/models/creatures/Robot.java
@@ -1,11 +1,29 @@
 package net.qmat.qmhh.models.creatures;
 
+import java.util.Vector;
+
+import net.qmat.qmhh.Main;
+
 import org.jbox2d.common.Vec2;
 
 public class Robot extends CreatureBase {
 
+	int num;
+	float pas;
+	public Vector<Cercle> cercles;
+
 	Robot() {
 		super();
+		num = (int)p.random(2, 6);
+		pas = 200f/num;
+
+		cercles = new Vector<Cercle>();
+		//points = new Vector<Poin>();
+
+		for(int a = 1; a < num; a ++)
+		{
+			cercles.add(new Cercle(a * pas, this));
+		}
 	}
 
 	public void draw() {
@@ -16,7 +34,7 @@ public class Robot extends CreatureBase {
 		 */
 		Vec2 position = box2d.getBodyPixelCoord(body);
 		float angle = body.getAngle();
-		
+
 		/*
 		 * w and h are also part of the CreatureBase class and they are
 		 * the width and height of the creature. The field p is a reference to
@@ -26,24 +44,98 @@ public class Robot extends CreatureBase {
 		p.pushMatrix();
 		p.translate(position.x, position.y);
 		p.rotateZ(angle);
-		p.rectMode(p.CENTER);
-		p.fill(100);
-		p.stroke(255);
-		p.rect(0, 0, w, h);
+
+		for(int a = 0; a < cercles.size(); a ++)
+		{
+			cercles.get(a).avance();
+		}
 		p.popMatrix();
-		
-		/*
-		 * Remember that all the constants and processing stuff is now part of p
-		 * or Main. For example, if you'd like to access TWO_PI you can so like 
-		 * this: Main.TWO_PI
-		 */
-		
-		/*
-		 * If you'd like to try out your creature change the 
-		 * preferences.properties file in /data/. Look for the PR_CREATURE_CLASS
-		 * setting and change it to your class' name. 
-		 */
 	}
-	
-	
+
+
+	public class Cercle
+	{
+		int n; 
+		float r, v, an, pas;
+		public Vector<Poin> mespoints;
+		float circRad = p.random(0f, 1f);
+
+		Cercle(float _r, Robot cf)
+		{
+			mespoints = new Vector<Poin>();
+			an = p.random(Main.TWO_PI);
+			v = p.radians(p.random(-0.8f, 0.8f));
+			r =_r;
+			n = (int)(((Main.PI * _r * 2f)/60f) + 1f);
+			pas = Main.TWO_PI/n;
+
+
+			for(int a = 0; a < n; a ++)
+			{
+				mespoints.add(new Poin(an + a * pas, circRad, this, cf));
+			}
+		}
+
+		void avance()
+		{
+			//		an += v;
+			for(int a = 0; a < mespoints.size(); a ++)
+			{
+				mespoints.get(a).avance(v);
+			}
+		}
+	}
+
+	class Poin
+	{
+		float an, r;
+		Cercle c;
+		float x, y;
+		//	int circRad = (int) random(5, 100);
+		Robot cf;
+
+		Poin(float _an, float _r, Cercle _c, Robot _cf)
+		{
+			r =_r; an = _an; c = _c; cf = _cf;
+		}
+
+		void avance(float _v)
+		{
+			an += _v;
+
+			x = Main.cos(an) * r * cf.w * 0.5f;
+			y = Main.sin(an) * r * cf.h * 0.5f;
+
+			for(int a = 0; a < cf.cercles.size(); a ++)
+			{
+				Cercle ci = cf.cercles.get(a);
+				if(ci != c)
+				{
+					if((c.r - ci.r) < 4*(stage+1))
+					{
+						for(int b = 0; b < ci.mespoints.size(); b ++)
+						{
+							if(getDistance(x, y, ci.mespoints.get(b).x, ci.mespoints.get(b).y) < 4*(stage+1))
+							{
+								p.stroke(255, p.random(60, 250));
+								p.strokeWeight(p.random(0, 2));
+								p.line(x, y, ci.mespoints.get(b).x, ci.mespoints.get(b).y);
+							}
+						}
+					}
+				}
+			}
+		}
+	}
+
+	float getDistance(float _x1, float _y1, float _x2, float _y2)
+	{
+		return Main.sqrt(Main.pow((_x1 -_x2), 2) + Main.pow((_y1 - _y2), 2));
+	}
+
+
+
+
 }
+
+
